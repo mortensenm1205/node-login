2c179ec18b5237535fac950368408dd774dcc220
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-01-31 12:19:43 -0700

    small schema and connection setup

---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/NodeLogin',
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    }
+);
+
+module.exports = { mongoose };
