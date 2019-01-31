47a2e1691b67a0c118a15b1d5099149dae3be308
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-01-31 14:36:15 -0700

    simple route that posts a user, and hashes the pw

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,21 @@ const router = require('express').Router();
 const { mongoose } = require('../config/db');
 const { User } = require('../models/user');
 
-router.post('/');
+router.post('/', (req, res) => {
+    // This is nested destructuring for objects. We are selectin { user }.
+    const { body: { user } } = req;
+    // Error handling for when email and password are blank values. 
+    if (!user.email) return res.status(400).send('Email is required');
+    if (!user.password) return res.status(400).send('Password is required');
 
-module.exports = { router };
+    const finalUser = new User(user);
+    finalUser.setPassHash(user.password)
+    return finalUser.save()
+        .then(() => res.status(200).json({ user: finalUser }))
+});
+
+router.get('/', (req, res) => {
+    res.send(req.headers);
+})
+
+module.exports =  router;
