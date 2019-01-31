d7ede75d6ac3a2c7a506937492727eb6db55a5fc
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-01-31 12:27:29 -0700

    comments for future use

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const app = express();
 
+// Will get cors deined when paried with cra so use cors()
 app.use(bodyParser.json(), cors());
 
 app.listen(3000, () => console.log('Now listening on port 3000'))
