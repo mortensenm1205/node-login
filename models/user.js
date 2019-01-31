42e9f0e4757b7438c6e9e4489896c4ebf895288b
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-01-31 12:27:18 -0700

    small, simple schema

--- models/user.js
+++ models/user.js
@@ -1,6 +1,9 @@
 const { Schema, model } = require('mongoose');
 
-const UserSchema = new Schema({});
+const UserSchema = new Schema({
+    email: String,
+    pass_hash: String
+});
 
 const User = model('User', UserSchema);
 
