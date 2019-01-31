60052be34ca5e1841253fb3df6c948569de5f215
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-01-31 12:19:34 -0700

    added cople more pcks, updated index

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json(), cors());
 
 app.listen(3000, () => console.log('Now listening on port 3000'))
