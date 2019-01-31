221ad755cd1688bd85be167710ed865592b53662
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-01-31 14:36:38 -0700

    adding two more methods for genJWT and setToken from genJWT

--- models/user.js
+++ models/user.js
@@ -1,16 +1,29 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new Schema({
     email: String,
-    pass_hash: String
+    password: String
 });
 
-UserSchema.methods.setPassHash = function(password) {
+UserSchema.methods.setPassHash = function(pass) {
+    // get and set for salt and hash
     const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(password, salt);
+    const hash = bcrypt.hashSync(pass, salt);
+    // updating the password value to the new hash value.
+    return this.password = hash;
+}
 
-    return this.pass_hash = hash;
+UserSchema.methods.generateJWT = function() {
+    return jwt({
+        _id: this._id,
+        email: this.email
+    }, 'secret');
+}
+
+UserSchema.methods.setToken = function() {
+    return this.token = this.generateJWT();
 }
 
 const User = model('User', UserSchema);
