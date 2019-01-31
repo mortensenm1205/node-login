d9b153d860f8504d65280f75188495a8b86c253b
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-01-31 12:49:30 -0700

    first method for creating pass_hash

--- models/user.js
+++ models/user.js
@@ -1,9 +1,17 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new Schema({
     email: String,
     pass_hash: String
 });
+
+UserSchema.methods.setPassHash = function(password) {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    return this.pass_hash = hash;
+}
 
 const User = model('User', UserSchema);
 
