b800bb6f1016dfc6d609451c45a203dba06bf48e
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-01-31 14:35:55 -0700

    some server config updates

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const user = require('./routes/user');
+
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Will get cors deined when paried with cra so use cors()
 app.use(bodyParser.json(), cors());
+app.use('/user', user);
 
-app.listen(3000, () => console.log('Now listening on port 3000'))
+app.listen(port, () => console.log(`Now listening on port ${port}`))
