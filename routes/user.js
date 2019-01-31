f5f3448e23b9a5a3a86e8839d73e35c94962c663
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-01-31 16:01:10 -0700

    testing auth/protected routes with express routes

--- routes/user.js
+++ routes/user.js
@@ -2,21 +2,27 @@ const router = require('express').Router();
 const { mongoose } = require('../config/db');
 const { User } = require('../models/user');
 
-router.post('/', (req, res) => {
+router.post('/create', (req, res) => {
     // This is nested destructuring for objects. We are selectin { user }.
     const { body: { user } } = req;
     // Error handling for when email and password are blank values. 
     if (!user.email) return res.status(400).send('Email is required');
     if (!user.password) return res.status(400).send('Password is required');
-
+    // This final is so that we can use the methods.
     const finalUser = new User(user);
     finalUser.setPassHash(user.password)
+    finalUser.setToken();
     return finalUser.save()
         .then(() => res.status(200).json({ user: finalUser }))
 });
 
-router.get('/', (req, res) => {
-    res.send(req.headers);
-})
+router.post('/login', (req, res)=> {
+    const { headers: { authorization } } = req;
+
+    if (authorization && authorization.split(' ')[0] === 'Token') {
+        console.log(authorization.split(' ')[1]);
+    }
+    console.log(null);
+});
 
 module.exports =  router;
