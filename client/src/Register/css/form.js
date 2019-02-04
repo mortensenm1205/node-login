58f5d1e9f57ed58c9fe08e7b4c869c23afd2982d
Matt Mortensen <mortensenm1205@gmail.com>
2019-02-04 12:21:48 -0700

    extending react-bs with styled-cmps

---
+++ client/src/Register/css/form.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { Form } from 'react-bootstrap';
+
+export const LayoutForm = styled(Form)`
+    width: 35%;
+`;
--- client/src/Register/index.js
+++ client/src/Register/index.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import { Form } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
+import { LayoutForm } from './css/form';
 
 class RegisterContainer extends Component {
     render() {
         return(
-            <form>
-                <input type="text" name="email" value="" />
-                <input type="text" name="password" value="" />
-                <button>Register</button>
-            </form>
+            <LayoutForm>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                     </Form.Text>
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Enter password" />
+                    <Form.Text className="text-muted">
+                        We'll never share your password with anyone else.
+                     </Form.Text>
+                </Form.Group>
+                <Button>Register</Button>
+            </LayoutForm>
         )
     }
 }
