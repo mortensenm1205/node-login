56a45c36c392b76f7fb1182a7137134c5c463457
Matt Mortensen <mortensenm1205@gmail.com>
2019-02-04 11:59:02 -0700

    updating for react-bs

--- client/src/Register/index.js
+++ client/src/Register/index.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { Form } from 'react-bootstrap';
 
 class RegisterContainer extends Component {
     render() {
         return(
-            <form>
-                <input type="text" name="email" value="" />
+            <Form>
+                {/* <input type="text" name="email" value="" />
                 <input type="text" name="password" value="" />
-                <button>Register</button>
-            </form>
+                <button>Register</button> */}
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                     </Form.Text>
+                </Form.Group>
+            </Form>
         )
     }
 }
