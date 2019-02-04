3759d67044f5bc1d411148ccb14e193c825c7262
Matt Mortensen <mortensenm1205@gmail.com>
2019-02-04 12:42:29 -0700

    setting client form comp done

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import Register from './Register';
 
 class App extends Component {
+
+  state = {
+    registerData: {}
+  }
+
+  change = e => {
+    const { registerData } = this.state;
+    let objToSend = { [e.target.name]: e.target.value };
+    this.setState({ registerData: { ...registerData, ...objToSend } })
+  }
+
+  submit = e => {
+    const { registerData } = this.state;
+    e.preventDefault();
+    e.target.reset();
+    console.log(registerData);
+  }
+
   render() {
     return (
       <div>
-        <Register />
+        <Register change={this.change} submit={this.submit} />
       </div>
     )
   }
--- client/src/Register/index.js
+++ client/src/Register/index.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
-import { Form, Button } from 'react-bootstrap';
-import { LayoutForm } from './css/form';
 
 class RegisterContainer extends Component {
     render() {
+        const { change, submit } = this.props;
         return(
-            <LayoutForm>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                     </Form.Text>
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Enter password" />
-                    <Form.Text className="text-muted">
-                        We'll never share your password with anyone else.
-                     </Form.Text>
-                </Form.Group>
-                <Button>Register</Button>
-            </LayoutForm>
+            <form onSubmit={submit}>
+                <input type="text" name="email" onChange={change} />
+                <input type="text" name="password" onChange={change} />
+                <button>Register</button>
+            </form>
         )
     }
 }
