379b8e83e3ba7256b1982b676f97cc2ba32fbf09
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-02-05 15:27:13 -0700

    using try/catch for better err handling on pw

--- routes/user.js
+++ routes/user.js
@@ -5,15 +5,27 @@ const { User } = require('../models/user');
 router.post('/create', (req, res) => {
     // This is nested destructuring for objects. We are selectin { user }.
     const { body } = req;
-    const finalUser = new User(body);
+    const user = new User(body);
 
-    finalUser.setPassHash(body.password)
-    finalUser.setToken();
-    return finalUser.save()
-        .then(() => res.status(200).json({ user: finalUser }))
-        .catch(e => {
-            res.status(400).json({ e })
-        })
+    // This handles errors for missing password. 
+    // We used a try/catch because of hashing the pw
+    // before saving the doc.
+    try {
+        user.setPassHash(body.password)
+        user.setToken();
+    } catch(e) {
+        let password = {
+            path: 'password',
+            message: 'Password is required.'
+        };
+        e.errors = { password };
+        return res.status(500).json({ e })
+    }
+
+    return user.save()
+        .then(() => res.status(200).json({ user }))
+        // This handles error for missing email.
+        .catch(e => res.status(400).json({ e }))
 });
 
 module.exports =  router;
