7444012f0b384060791ddd18fac4cb29a9507604
Matt Mortensen <mortensenm1205@gmail.com>
2019-02-07 07:06:52 -0700

    more testing on registration errors

--- routes/user.js
+++ routes/user.js
@@ -5,28 +5,31 @@ const { User } = require('../models/user');
 router.post('/create', (req, res) => {
     // This is nested destructuring for objects. We are selectin { user }.
     const { body } = req;
+    const user = new User(body);
+
     // This handles errors for missing password. 
     // We used a try/catch because of hashing the pw
     // before saving the doc.
-    try {
-        const user = new User(body);
-    } catch(e) {
-        if (body.password === '') {
-            let password = {
-                path: 'password',
-                message: 'Password is required.'
-            };
-            return e.errors = { password };
-        }
-        return res.status(400).json({ e })
-    }
 
     user.setPassHash(body.password)
     user.setToken();
+ 
+
     return user.save()
         .then(() => res.status(200).json({ user }))
         // This handles error for missing email.
-        .catch(e => res.status(400).json({ e }))
+        .catch(e => {
+            // res.status(400).json({ e })
+            if (body.password === '' && body.email !== '') {
+                let password = {
+                    message: `Path 'password' is required`,
+                    path: 'password'
+                }
+                e.errors = password;
+                console.log(e.errors);
+            }
+
+        })
 });
 
 module.exports =  router;
