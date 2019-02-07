c03ff9522348760e17a5e060948b542856ae97d1
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-02-07 14:57:12 -0700

    Register comp now display error messages for empty fields

--- client/src/Register/index.js
+++ client/src/Register/index.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 
 class RegisterContainer extends Component {
     render() {
-        const { change, submit, value } = this.props;
+        const { change, submit, value, error } = this.props;
+        // Since user shares success and error data. 
+        // I needed to write a condition that checks to see if the 
+        // error object is there. 
+        if (error.hasOwnProperty('isError')) {
+            var passwordError = error.data.code === 'password' ? <p>{error.data.message}</p> : null;
+            var emailError = error.data.code === 'email' ? <p>{error.data.message}</p> : null;
+        }
         return(
-            <form onSubmit={submit}>
-                <input type="text" name="email" onChange={change} value={value.email} />
-                <input type="text" name="password" onChange={change} value={value.password} />
-                <button>Register</button>
-            </form>
+            <div>
+                <form onSubmit={submit}>
+                    <input type="text" name="email" onChange={change} value={value.email} />
+                    <input type="text" name="password" onChange={change} value={value.password} />
+                    <button>Register</button>
+                </form>
+                <div>
+                    {passwordError}
+                    {emailError}
+                </div>
+            </div>
         )
     }
 }
