fb1c957df301dacad7563a6ca85d5da5b7c9174a
Matt Mortensen <mortensenm1205@gmail.com>
2019-02-07 22:47:37 -0700

    converted to presentational comp

--- client/src/Register/index.js
+++ client/src/Register/index.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class RegisterContainer extends Component {
-    render() {
-        const { change, submit, value, error } = this.props;
-        // Since user shares success and error data. 
-        // I needed to write a condition that checks to see if the 
-        // error object is there. 
-        if (error.hasOwnProperty('isError')) {
-            var passwordError = error.data.code === 'password' ? <p>{error.data.message}</p> : null;
-            var emailError = error.data.code === 'email' ? <p>{error.data.message}</p> : null;
-        }
-        return(
-            <div>
-                <form onSubmit={submit}>
-                    <input type="text" name="email" onChange={change} value={value.email} />
-                    <input type="text" name="password" onChange={change} value={value.password} />
-                    <button>Register</button>
-                </form>
-                <div>
-                    {passwordError}
-                    {emailError}
-                </div>
-            </div>
-        )
+const RegisterContainer = ({ change, submit, value, error }) =>{ 
+    // Since user shares success and error data. 
+    // I needed to write a condition that checks to see if the 
+    // error object is there. 
+    if (error.hasOwnProperty('isError')) {
+        var passwordError = error.data.code === 'password' ? <p>{error.data.message}</p> : null;
+        var emailError = error.data.code === 'email' ? <p>{error.data.message}</p> : null;
     }
+    return(
+        <div>
+            <form onSubmit={submit}>
+                <input type="text" name="email" onChange={change} value={value.email} />
+                <input type="text" name="password" onChange={change} value={value.password} />
+                <button>Register</button>
+            </form>
+            <div>
+                {passwordError}
+                {emailError}
+            </div>
+        </div>
+    )
 }
+    
 
 export default RegisterContainer;
