e9a05d43fca4ba40ff3ebc204c8560ee34551a13
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-02-20 09:21:43 -0700

    removing register func into register comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,18 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { sendNewUser } from './Register/actions';
 import Register from './Register';
 import Login from './Login';
 
 class App extends Component {
 
-  state = {
-    registerData: {
-      // Including the specific field names did help with reseting. 
-      email: '',
-      password: ''
-    }
-  }
-
-  change = e => {
-    const { registerData } = this.state;
-    let objToSend = { ...registerData, [e.target.name]: e.target.value };
-    this.setState({ registerData: objToSend })
-  }
-
-  submit = e => {
-    const { registerData } = this.state;
-    const { sendUser } = this.props;
-    e.preventDefault();
-    // Need to clear the field values once user hits the submit btn
-    this.setState({ registerData: { email: '', password: '' } })
-    sendUser(registerData);
-  }
-
   render() {
-    const { registerData } = this.state;
-    const { user } = this.props;
     return (
       <div>
         <Switch>
           <Route path='/register' render={
             rest => {
               return ( 
-                <Register 
-                  change={this.change} 
-                  submit={this.submit} 
-                  value={registerData} 
-                  /* Since my reducer for user contains success and errors i have to pass in user as an error prop */
-                  error={user} 
-                  {...rest}
-                />
+                <Register {...rest} />
               )
             }
           }/>
@@ -63,16 +29,4 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    user: state.userData
-  }
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    sendUser: user => dispatch(sendNewUser(user))
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
