0f54aaa161c449c12e296f7dbaa2abacfb5f8c9f
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-02-20 10:01:39 -0700

    refactoring router for cleaner code

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,12 @@ import Register from './Register';
 import Login from './Login';
 
 class App extends Component {
-
   render() {
     return (
       <div>
         <Switch>
-          <Route path='/register' render={
-            rest => {
-              return ( 
-                <Register {...rest} />
-              )
-            }
-          }/>
-          <Route path='/login' render={
-            rest => {
-              return (
-                <Login {...rest} />
-              )
-            }
-          }/>
+          <Route path='/register' component={ Register } />
+          <Route path='/login' component={ Login } />
         </Switch>
       </div>
     )
