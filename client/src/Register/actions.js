8364244008bde03757641cce20ef79ee8b2ae9ad
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-02-21 13:23:37 -0700

    cleanup for easier to read action

--- client/src/Register/actions.js
+++ client/src/Register/actions.js
@@ -1,18 +1,13 @@
 import axios from 'axios';
 import { userError } from '../config/genActions'; 
 
-export const createNewUser = (new_user, history) => {
-    const { email, password } = new_user;
-    return dispatch => {
-        axios.post('/user/create', { email, password})
-            // My error handling is done via the server
-            .then(res => {
-                const { user } = res.data;
-                console.log(user);
-                // Saving to localStorage for later use.
-                localStorage.setItem('jwt', 'Bearer ' + user.token)
-                history.push('/');
-            })
-            .catch(e => dispatch(userError(e.response.data.e)))
-    }
+export const createNewUser = (new_user, history) => dispatch => {
+    axios.post('/user/create', new_user)
+        // My error handling is done via the server
+        .then(res => {
+            // Saving to localStorage for later use.
+            localStorage.setItem('jwt', 'Bearer ' + res.data.user.token)
+            history.push('/');
+        })
+        .catch(e => dispatch(userError(e.response.data.e)))
 }
