226c0459850a0426f6b005d7214b0e737fa3be3a
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-02-21 12:57:15 -0700

    login comp now full functional

--- client/src/Login/index.js
+++ client/src/Login/index.js
@@ -1,14 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { loginUser } from './actions';
 
-const LoginContainer = props => {
-    return (
-        <form>
-            <input type="text" name="email" />
-            <input type="text" name="password" />
-            <button>Login</button>
-        </form>        
-    )
+class LoginContainer extends Component {
+
+    state = {
+        loginData: {
+            // Including the specific field names did help with reseting. 
+            email: '',
+            password: ''
+        },
+        errors: {}
+    }
+
+    // This is to help set errors from props onto state, 
+    // without relying solely on a conditional render like before.
+    // Using lc methods and conditions is best practice to set props
+    // to state.
+    static getDerivedStateFromProps(nextProps, prevState) {
+        if (nextProps.errors !== prevState.errors) {
+            return { errors: nextProps.errors }
+        }
+        else return null;
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.errors !== this.state.errors) {
+            this.setState({ errors: prevProps.errors })
+        }
+    }
+
+    change = e => {
+        const { loginData } = this.state;
+        let objToSend = { ...loginData, [e.target.name]: e.target.value };
+        this.setState({ loginData: objToSend })
+    }
+
+    submit = e => {
+        const { loginData } = this.state;
+        const { login } = this.props;
+        e.preventDefault();
+        // Need to clear the field values once user hits the submit btn
+        this.setState({ loginData: { email: '', password: '' } })
+        login(loginData);
+    }
+
+    render() {
+        const { errors, loginData } = this.state;
+        const { email, password } = loginData;
+        return (
+            <div>
+            <form onSubmit={this.submit}>
+                <input type="text" name="email" onChange={this.change} value={email}/>
+                <input type="password" name="password" onChange={this.change} value={password}/>
+                <button>Login</button>
+            </form> 
+                <div>
+                    {errors.data && errors.data.code === "blankEmail" && <div>{errors.data.message}</div>}
+                    {errors.data && errors.data.code === "blankPassword" && <div>{errors.data.message}</div>}
+                    {errors.data && errors.data.code === "noFoundUser" && <div>{errors.data.message}</div>}
+                    {errors.data && errors.data.code === "wrongPassword" && <div>{errors.data.message}</div>}
+                </div>
+            </div>       
+        )
+    }
 }
 
 
-export default LoginContainer;
+const mapStateToProps = state => {
+    return {
+        errors: state.errors
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        login: user => dispatch(loginUser(user))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginContainer);
