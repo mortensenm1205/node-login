7c8cb980bbacd2344b8994ad8d40117aae13a8a2
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-02-21 13:52:48 -0700

    using util funct

--- client/src/Register/actions.js
+++ client/src/Register/actions.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { setAuthToken } from '../config/setAuthToken';
 import { userError } from '../config/genActions'; 
 
 export const createNewUser = (new_user, history) => dispatch => {
@@ -6,7 +7,8 @@ export const createNewUser = (new_user, history) => dispatch => {
         // My error handling is done via the server
         .then(res => {
             // Saving to localStorage for later use.
-            localStorage.setItem('jwt', 'Bearer ' + res.data.user.token)
+            localStorage.setItem('jwt', 'Bearer ' + res.data.user.token);
+            setAuthToken(res.data.user.token);
             history.push('/');
         })
         .catch(e => dispatch(userError(e.response.data.e)))
