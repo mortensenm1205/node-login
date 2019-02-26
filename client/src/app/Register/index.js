2a7f2be07d8f04d5286cb0a0f9aa63844b9b0337
Matt Mortensen <mortensenm1205@gmail.com>
2019-02-26 14:13:36 -0700

    ducks for Login complete

--- client/src/app/Register/index.js
+++ client/src/app/Register/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { createNewUser } from './actions';
+import { createNewUser } from './ducks/actions';
 
 class RegisterContainer extends Component {
 
