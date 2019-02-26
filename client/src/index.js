7122df8213c0ca939f8e161154964f8aeb7a65a9
Matt Mortensen <mortensenm1205@gmail.com>
2019-02-26 14:09:53 -0700

    moving App to app dir

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './app/App';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createBrowserHistory }  from 'history'
