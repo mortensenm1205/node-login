54c55ce54e2d291b10c4ab70772d75ce0d474ec0
Matt Mortensen <mortensenm1205@gmail.com>
2019-02-27 15:30:45 -0700

    updating errors for heroku app

--- client/src/config/store.js
+++ client/src/config/store.js
@@ -6,7 +6,7 @@ import {
 } from 'redux';
 
 import thunk from 'redux-thunk';
-import { userErrorData, userData } from '../app/ducks/user/actions';
+import { userErrorData, userData } from '../app/ducks/user/reducers';
 
 const rootReducer = combineReducers({
     user: userData,
