38d0d7779d23949e7ccc298cf77425b873a71e35
Matt Mortensen <mortensenm1205@gmail.com>
2019-02-27 14:47:17 -0700

    final fail safe for checking auth when logged in done

--- client/src/app/Home/index.js
+++ client/src/app/Home/index.js
@@ -5,37 +5,35 @@ import { logoutUser } from './ducks/actions';
 import { userData } from '../ducks/user/actions';
 import { setAuthToken } from '../../config/setAuthToken';
 
-
-
 class HomeContainer extends Component {
   
   componentDidMount() {
+    const { logout, history, getUser, user } = this.props;
     if (localStorage.jwt) {
-        const { logout, history, user } = this.props;
         const currentTime = Date.now() / 1000;
         // We do the same thing in our authentication action call.
         setAuthToken(localStorage.jwt);
-    
+        getUser();
+
         // This is setting an expiration date on someone
         // being logged in. Notice that the user still
         // has to be logged for this to happen. 
         if (user.exp < currentTime) {
             logout(history);
         }
-    }
-    const { getUser } = this.props;
-    getUser();
+    } else history.push('/login');
+
   }
 
-    render() {
-      const { user, logout, history } = this.props;
-        return(
-            <div>
-              {console.log(user)}
-              {user.isAuth && <button onClick={() => logout(history)}>Logout</button>}
-            </div>
-        )
-    }
+  render() {
+    const { user, logout, history } = this.props;
+      return(
+          <div>
+            {console.log(user)}
+            {user.isAuth && <button onClick={() => logout(history)}>Logout</button>}
+          </div>
+      )
+  }
 }
 
 const mapStateToProps = state => {
