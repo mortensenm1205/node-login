724fe6e45c11be5bfb9ebbeac029ff0a7a2b0cc1
Matt Mortensen <mortensenm1205@gmail.com>
2019-02-28 11:39:50 -0700

    applying new common css styles, new bug bleh

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-
+import { Form } from './css';
+ 
 import Home from './Home';
 import Register from './Register';
 import Login from './Login';
@@ -11,12 +12,28 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path='/' component={ Home } />
-          <Route path='/register' component={ Register } />
-          <Route path='/login' component={ Login } />
+          {/* Using the stateless comp here, noticed i don't have a path prop. This is due to the stateless component containing the routes within. */}
+          <Route component={ FormComps } />
         </Switch>
       </div>
     )
   }
+}
+
+/*
+  Grouping this routes into a stateless component
+  so that i may apply common style attributes between
+  the two routed components with affecting the Switch comp.
+*/
+const FormComps = () => {
+  return (
+    <Form>
+      <Switch>
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+      </Switch>
+    </Form>
+  )
 }
 
 export default App;
