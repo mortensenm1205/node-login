b0f22d12abede553444176b3b4ff148ac0eea721
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-03-04 12:42:35 -0700

    moving grid away from mobile first. Not needed

--- client/src/app/css/index.js
+++ client/src/app/css/index.js
@@ -4,12 +4,12 @@ export const App = styled.div`
 `;
 
 export const Form = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
     margin: 0 auto;
     max-width: 85%; 
-    
+
     @media(min-width: 800px) {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
         max-width: 45%;
     }
 `;
