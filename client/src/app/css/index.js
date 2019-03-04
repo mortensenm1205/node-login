a050d3bb60e2509ef231b9396a086a769c34fa33
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-03-04 15:30:33 -0700

    moved base styles out of html  into styled comps

--- client/src/app/css/index.js
+++ client/src/app/css/index.js
@@ -1,15 +1,40 @@
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 
+// This is being used in index.js outside of /app
+// This is replacing what was in the index.html file.
+export const Base = createGlobalStyle`
+    html, body {
+        background-color: #434857;
+        font-family: 'Roboto', sans-serif;
+        font-weight: 500;
+        font-size: 18px;
+        color: #efeeee;
+      }
+
+    html, body, h1, h2, h3, section, p {
+        margin: 0;
+    }
+`
+
+// App is our root container, 
+// setting up grid here only after 800px screen size. 
 export const App = styled.div`
-`;
-
-export const Form = styled.div`
     margin: 0 auto;
-    max-width: 85%; 
 
     @media(min-width: 800px) {
         display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        max-width: 45%;
+        grid-template-rows: repeat(2, 1fr);
+    }
+`;
+
+// I'm creating this comp because 
+// /register and /login share certain styles 
+// due to both being form comps.
+export const Form = styled.div`
+    margin: 0 auto;
+    width: 90%;
+
+    @media(min-width: 800px) {
+        width: 35%;
     }
 `;
