629815a4cf50c9e0df005da79ccb9044708ae594
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-03-04 12:43:07 -0700

    styles for home comp

---
+++ client/src/app/Home/css/index.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const Home = styled.div`
+    height: 100vh;
+    background-color: ${props => props.auth && "#fff;"}
+    text-align: center;
+`;
+
+export const HomeTitle = styled.h1`
+    color: #000;
+    margin: 0 0 5%;
+    `;
+
+export const HomeInfo = styled.p`
+    color: #000;
+    font-size: 1.2em;
+    margin: 0 0 3.5%;
+`;
+
+export const HomeButton = styled.button`
+    box-shadow: 4px 7px 37px -12px #f8ad1c;
+    border: none;
+    padding: 4% 11%;
+    border-radius: 25px;
+    font-size: 1.2em;
+    font-weight: 700;
+    background-color: #f8ad1ce8;
+    color: #fff;
+    width: 95%;
+
+    :focus { 
+        outline: 0;
+    }
+`;
