299ada5fcce7dff7a94be82cba51fc6a20602d24
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-03-04 15:31:22 -0700

    refactoring to include base comp

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './app/App';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createBrowserHistory }  from 'history'
+import { createBrowserHistory }  from 'history';
+import { Base } from './app/css';
 import store from './config/store';
 import * as serviceWorker from './serviceWorker';
 
@@ -12,6 +13,8 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
     <Router history={createBrowserHistory()}>
         <Provider store={store}>
+            {/* Base is our global styles, however this doesn't render children so treat it as an indivdual, seperate comp */}
+            <Base />
             <App />
         </Provider>
     </Router>, 
